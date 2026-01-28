b61fb56eb6289bfd7321c13b1a75507ff2d325db
NavidMoeinDarbari <navidoomoeindarbar77@gmail.com>
2026-01-28 12:47:25 +0330

    Commit9:v8: few bugs fixed

--- src/app/dashboard/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserAdditionalInfo } from '../../authentication/user.model';
+import { AuthService } from '../../authentication/auth.service';
 
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
-  styleUrl: './dashboard-layout.component.css'
+  styleUrl: './dashboard-layout.component.css',
 })
 export class DashboardLayoutComponent {
+  constructor(public authService: AuthService) {}
+  userInfo$!: Observable<UserAdditionalInfo>;
 
+  ngOnInit(): void {
+    this.userInfo$ = this.authService.userIdSubject.asObservable();
+  }
 }
